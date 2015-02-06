adafc04b3811f50efc54ddb8cd22778f5e8f618c
Michael Raimi <mar2260@columbia.edu>
2015-02-06 16:53:16 -0500

    updated params to be general

--- hw1_train1a.m
+++ hw1_train1a.m
@@ -1,20 +1,20 @@
 function params = hw1_train1a(X,Y)
-    load('mnist.mat');
+    load('hw1_data.mat');
     pi = zeros(10,1);
     for q=1:length(Y)
             pi((Y(q)+1),1) = pi((Y(q)+1),1) + 1;
     end
     pi = pi/length(Y);
  
-    mu = zeros(10,784);
+    mu = zeros(10,size(X,2));
     for q=1:length(Y)
             mu((Y(q)+1),:) =  mu((Y(q)+1),:) + X(q,:)/pi((Y(q)+1),1);
     end
     mu = mu/length(Y);
     
-    diff = zeros(length(Y),784);
-    sig = zeros(784,784);
-    mu_y = zeros(1,784);
+    diff = zeros(length(Y),size(X,2));
+    sig = zeros(size(X,2), size(X,2));
+    mu_y = zeros(1,size(X,2));
     pi_y = zeros(1,1);
     class = struct('pi', pi_y, 'mu', mu_y, 'sigma', sig);
     params(10,1) = class;
--- run.m
+++ run.m
@@ -1,2 +1,2 @@
-load('mnist.mat');
+load('hw1.data.mat');
 hw1_test1a (hw1_train1a(data, labels), testdata);
