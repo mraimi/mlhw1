389d2c7e06948a470e01ebc0b5aa830fc06e3f90
Michael Raimi <mar2260@columbia.edu>
2015-02-06 13:45:32 -0500

    first commit

---
+++ hw1_data.m
@@ -0,0 +1,16 @@
+function [X,Y] = hw1_data(n)
+colormap(1-gray);
+imagesc(reshape(data(3,:),28,28)');
+
+rng('shuffle');
+Y = randsample([0;1;2],n,1);
+X = zeros(n,2);
+
+means = [0,0; 2,0; 0,2];
+stddevs = [1, 1/2, 1];
+for k=1:3
+	m = sum(Y==mod(k,3));
+	X(Y==mod(k,3),:) = bsxfun(@plus,stddevs(k)*randn(m,2),means(k,:));
+end
+end
+
