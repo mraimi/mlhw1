83f66e19ef6d4a4029925196c63f603bf865f468
Michael Raimi <mar2260@columbia.edu>
2015-02-06 19:11:18 -0500

    broken. testing classifier fn

--- hw1_test1a.m
+++ hw1_test1a.m
@@ -1,4 +1,22 @@
 function preds = hw1_test1a(params,test)
+   prediction = zeros(10,1);
+   preds(length(test), 1) = -1;
    
+    for i=1:length(test)
+        x = test(i,:);
+        highest_index = -1;
+        for y=1:10
+           scale = params(y,1).pi(y)/sqrt(det(params(y,1).pi(y)));
+           ex = exp((-1/2)*(x - (params(y,1).mu))*pinv(params(y,1).sigma)*(x - (params(y,1).mu))');
+           prediction(y,1) = scale*ex;
+        end
+        for y=1:10
+            if prediction(y,1) > highest_index
+                highest_index = y;
+        end
+        preds(i, 1) = highest_index;
+        highest_index = -1;
+        prediction = zeros(10,1);
+        end
 end
 
--- run.m
+++ run.m
@@ -1,2 +1,1 @@
-load('hw1.data.mat');
 hw1_test1a (hw1_train1a(data, labels), testdata);
